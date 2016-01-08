bf3b54d1ac22807214cf4fdf6a71ae842264108c
Rainer Sieger <rsieger@pangaea.de>
2016-01-08 13:56:35 +0100

    _VERSION_ updated

--- trunk/Headers/Globals.h
+++ trunk/Headers/Globals.h
@@ -7,21 +7,21 @@
     const QString _PROGRAMDOI_          = "";
     const QString _PROGRAMREADME_       = "http://wiki.pangaea.de/wiki/PanUploader";
 
-    const QString _VERSION_	            = "2.7"; // Version number, 2015-12-28
-    const QChar	  _PSEPARATOR_			= '|';
+    const QString _VERSION_             = "2.7"; // Version number, 2015-12-28
+    const QChar   _PSEPARATOR_          = '|';
 
-    const int	_NOERROR_				= 0;
-    const int	_ERROR_					= 1;
-    const int	_APPBREAK_				= 2;
-    const int	_DATAFOUND_				= 4;
+    const int   _NOERROR_               = 0;
+    const int   _ERROR_                 = 1;
+    const int   _APPBREAK_              = 2;
+    const int   _DATAFOUND_             = 4;
     const int   _CHOOSEABORTED_         = 5;
     const int   _FILENOTEXISTS_         = 6;
     const int   _DONE_                  = 7;
 
-    const int	_NODATAFOUND_			= -3;
+    const int   _NODATAFOUND_           = -3;
 
-    const int	_ZIP_					= 1;
-    const int	_GZIP_					= 2;
+    const int   _ZIP_                   = 1;
+    const int   _GZIP_                  = 2;
 
     const int   _HSSRV2_                = 1;
     const int   _MW1_                   = 2;
@@ -49,13 +49,13 @@
     const int   _CSV_                   = 1;
 
 // Filenames
-    const QString _COREDESCRIPTION_FILENAME_	 = "CoreDescription.txt";
-    const QString _CORELEGEND_FILENAME_			 = "CoreLegend.txt";
+    const QString _COREDESCRIPTION_FILENAME_     = "CoreDescription.txt";
+    const QString _CORELEGEND_FILENAME_          = "CoreLegend.txt";
     const QString _CORELOGGERPROTOKOLL_FILENAME_ = "CoreLoggerProtokoll.txt";
     const QString _CORESECTIONTABLE_FILENAME_    = "CoreSectionTable.txt";
-    const QString _BENTHOSMAP_FILENAME_			 = "BenthosMap.txt";
-    const QString _BENTHOSTRACK_FILENAME_		 = "BenthosTrack.txt";
-    const QString _BENTHOSBATH_FILENAME_		 = "BenthosBath.txt";
-    const QString _INFO_FILENAME_				 = "Info.txt";
+    const QString _BENTHOSMAP_FILENAME_          = "BenthosMap.txt";
+    const QString _BENTHOSTRACK_FILENAME_        = "BenthosTrack.txt";
+    const QString _BENTHOSBATH_FILENAME_         = "BenthosBath.txt";
+    const QString _INFO_FILENAME_                = "Info.txt";
 
 #endif
