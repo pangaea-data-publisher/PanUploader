d3771070d10db0cf8872dddb403d577c8d379b55
Rainer Sieger <rsieger@pangaea.de>
2017-10-06 19:03:48 +0200

    Qt 5.9.1 -> Qt 5.9.2

--- macdeploy.sh
+++ macdeploy.sh
@@ -7,10 +7,10 @@ echo - macdeployqt
 cd ~/Development/GitHub/PanUploader
 
 rm -R '../../Distribution/PanUploader/PanUploader.app'
-cp -R './build-PanUploader-Desktop_Qt_5_9_1_clang_64bit-Release/PanUploader.app' '../../Distribution/PanUploader/PanUploader.app'
+cp -R './build-PanUploader-Desktop_Qt_5_9_2_clang_64bit-Release/PanUploader.app' '../../Distribution/PanUploader/PanUploader.app'
 cp './trunk/Resources/Info.plist' '../../Distribution/PanUploader/PanUploader.app/Contents/Info.plist'
 
-/Developer/Qt/5.9.1/clang_64/bin/macdeployqt '../../Distribution/PanUploader/PanUploader.app'
+/Developer/Qt/5.9.2/clang_64/bin/macdeployqt '../../Distribution/PanUploader/PanUploader.app'
 
 echo - code signing
 
